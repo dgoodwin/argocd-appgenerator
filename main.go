5b702ae6fde8ee58171fef16bae8f2cfa75a438e
Devan Goodwin <dgoodwin@redhat.com>
2020-02-07 14:22:13 -0400

    Add scaffolding for ApplicationGenerator CRD.

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	appgeneratorrmrfcav1 "github.com/dgoodwin/argocd-appgenerator/api/v1"
+	"github.com/dgoodwin/argocd-appgenerator/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +37,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = appgeneratorrmrfcav1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -61,6 +64,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ApplicationGeneratorReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("ApplicationGenerator"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ApplicationGenerator")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
